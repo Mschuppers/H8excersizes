5c051a1a9c700fc71df9cce809b89dbd85663939
Mschuppers <m.schuppers@belastingdienst.nl>
2021-09-20 13:55:08 +0200

    .

--- src/main/java/Persoon.java
+++ src/main/java/Persoon.java
@@ -1,3 +1,83 @@
 public class Persoon {
+
+    private int age;
+    private String universalRights = "All humans are equal";
+    //private Name name;
+    private Gender gender;
+    private static final int maxAge = 131;
+
+    private String name;
+//    private enum Name {
+//        UNKOWN,
+//        JAN_JANSSEN
+//    }
+
+    private enum Gender {
+        MALE,
+        FEMALE
+    }
+
+    //CONSTRUCTORS
+    public Persoon(String name, int age, Gender gender) {
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+    }
+
+    //SETTERS
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    //GETTERS
+
+
+    //MAIN
+    public static void main(String[] args) {
+        //Generate objects
+        Persoon p = new Persoon("Jan Janssen", 42, Gender.MALE);
+        Persoon q = new Persoon("Jan Janssen", 42, Gender.MALE);
+
+        //toString the object details
+        System.out.println(p);
+        System.out.println(q);
+
+        //Compare objects
+        if (p.equals(q)) {
+            System.out.println("Equal ");
+        } else {
+            System.out.println("Not Equal ");
+        }
+        System.out.println(p.hashCode());
+        System.out.println(q.hashCode());
+
+    }
+
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.age + " " + this.gender;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        // typecast o to Persoon
+        Persoon c = (Persoon) o;
+
+        // Check if o is an instance of Persoon
+        if (!(o instanceof Persoon)) {
+            return false;
+        }
+        return ((Persoon) o).age == age && ((Persoon) o).name == name;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = age * name.hashCode() * gender.hashCode();
+        return result;
+    }
 }
-git
+
